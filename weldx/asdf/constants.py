3b25e3af1312b6c1581d1d271b49f0f2262041df
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-05-06 20:03:08 +0200

    all

--- weldx/asdf/constants.py
+++ weldx/asdf/constants.py
@@ -3,6 +3,13 @@ from pathlib import Path
 
 from asdf.util import filepath_to_url
 
+__all__ = [
+    "SCHEMA_PATH",
+    "WELDX_SCHEMA_URI_BASE",
+    "WELDX_TAG_BASE",
+    "WELDX_URL_MAPPING",
+]
+
 SCHEMA_PATH = str(Path(__file__).resolve().parents[0] / "schemas")
 WELDX_SCHEMA_URI_BASE = "http://weldx.bam.de/schemas/"
 WELDX_TAG_BASE = "tag:weldx.bam.de:weldx"
